5f64acde80fb457fffda98ebc3f5afe863422100
Catherine Clause <cnclause123@gmail.com>
2019-08-18 19:36:40 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,7 +9,9 @@ end
 introduction("Josh") 
 
 def introduction_with_language(name, language)
-  puts ""
+  puts "Hi, my name is #{name} and I am learning to program in #{language}" 
+end 
+introduction_with_language("")
 
 
 
